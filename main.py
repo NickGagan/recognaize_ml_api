f008ac47d9a3e1aca1cc5d5c044ab9bd961adaac
Nick Gagan <njgagan@gmail.com>
2022-03-08 20:59:15 -0500

    production setup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import nltk
 from collections import Counter
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -9,6 +9,9 @@ import pandas as pd
 from gensim.models.doc2vec import Doc2Vec
 import csv
 import numpy as np
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
 
 # Downloads needed for lemmatization
 nltk.download('wordnet')
@@ -21,6 +24,9 @@ with open('./models/BoW.csv', newline='') as f:
     BoW = (np.array(list(reader))).flatten()
 
 app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
 
 # Load models
 tfidf = joblib.load("./models/tfidf.pkl")
@@ -61,37 +67,41 @@ def getDoc2Vec(text):
     vector = d2vModel.infer_vector(text.split(" "))
     return vector.tolist()
 
+class aiModel(Resource):
+    def post(self):
+        # POS tagging
+        data = request.json
+        data_type = (request.args).get("type")
+        data["clean_text"] = ' '.join([wn.lemmatize(word) for word in data["clean_text"].split(' ')])
+        data["unique_clean_text"] = " ".join(dict.fromkeys(data["clean_text"].split()))
+        data["word_count"] = len(data["unique_clean_text"].split())
 
-@app.route("/", methods=["POST"])
-def run_model():
-    # POS tagging
-    data = request.json
-    data_type = (request.args).get("type")
-    data["clean_text"] = ' '.join([wn.lemmatize(word) for word in data["clean_text"].split(' ')])
-    data["unique_clean_text"] = " ".join(dict.fromkeys(data["clean_text"].split()))
-    data["word_count"] = len(data["unique_clean_text"].split())
+        pos_counts = posCount(data["unique_clean_text"], data["word_count"])
 
-    pos_counts = posCount(data["unique_clean_text"], data["word_count"])
+        data["norm_noun"] = pos_counts[0]
+        data["norm_verb"] = pos_counts[1]
+        data["norm_adj"] = pos_counts[2]
+        data["norm_adv"] = pos_counts[3]
+        data["BOW"] = {}
 
-    data["norm_noun"] = pos_counts[0]
-    data["norm_verb"] = pos_counts[1]
-    data["norm_adj"] = pos_counts[2]
-    data["norm_adv"] = pos_counts[3]
-    data["BOW"] = {}
+        # sentiment analysis
+        va = SentimentIntensityAnalyzer()
+        data["compound_sent"] =  va.polarity_scores(data["clean_text"])['compound']
 
-    # sentiment analysis
-    va = SentimentIntensityAnalyzer()
-    data["compound_sent"] =  va.polarity_scores(data["clean_text"])['compound']
+        # Run TFIDF
+        X_tf = tfidf.transform([data["clean_text"]])
+        data = processTFIDF(data, X_tf)
 
-    # Run TFIDF
-    X_tf = tfidf.transform([data["clean_text"]])
-    data = processTFIDF(data, X_tf)
+        # Process Doc2Vec
+        data["post2vec"] = getDoc2Vec(data["clean_text"])
+        # Pass to model
+        # TODO Get results from model
+        results = {
+            "poor_mental_health": True
+        }
+        return (jsonify(results))
 
-    # Process Doc2Vec
-    data["post2vec"] = getDoc2Vec(data["clean_text"])
-    # Pass to model
-    # TODO Get results from model
-    results = {
-        "poor_mental_health": True
-    }
-    return (results)
+api.add_resource(aiModel, '/')
+
+if __name__ == '__main__':
+    app.run()
